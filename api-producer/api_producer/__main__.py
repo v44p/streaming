3e5a0535d39076f05946ba7f72141d33b0cca744
vlad <vladimir.prosser@retargetly.com>
2020-03-03 18:06:48 -0300

    se crea un producer en python

--- api-producer/api_producer/__main__.py
+++ api-producer/api_producer/__main__.py
@@ -1,18 +1,12 @@
-import time
-
 from api_producer.objects import Producer
 from api_producer.config import *
 
 RETURN_EXITOSO = True
 
-
 def main():
 
     producer = Producer()
-    for i in range(10):
-        producer.post_message(i)
-        time.sleep(2)
-    
+    producer.post_message()
     return RETURN_EXITOSO
 
 
--- api-producer/api_producer/objects.py
+++ api-producer/api_producer/objects.py
@@ -1,18 +1,23 @@
 import os
 from json import dumps
+import time
 
 from kafka import KafkaProducer
 from datetime import datetime
 
 class Producer:
 
-    def post_message(self, m):
+    def post_message(self):
         producer = KafkaProducer(
             value_serializer=lambda m: dumps(m).encode('utf-8'), 
             bootstrap_servers=['kafka1:9092','kafka2:9093','kafka3:9094']
         )
-        print('post ...')
-        producer.send("test", value={"hello": "producer_%s".format(m)})
+        i = 0
+        while True:
+            print('post ...')
+            producer.send("test", value={"hello": "producer_%s".format(i)})
+            i = i + 1
+            time.sleep(5)
 
         return True
 
